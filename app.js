0e4168350d8b7c01979338799c732a2862a00134
Daniel Eicher <d.eicher@mailbox.org>
2022-04-20 18:46:30 +0200

    Updating the health bars

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = Vue.createApp({
       playerHealth: 100,
     }
   },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + '%' }
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + '%' }
+    }
+  },
   methods: {
     attackMonster() {
       const attackValue = getRandomValue(5, 12)
