b8735314239f2d18a1ab69b2d5962709aaabfe20
Daniel Eicher <d.eicher@mailbox.org>
2022-04-20 20:28:50 +0200

    Add a game over screen

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
     return {
       monsterHealth: 100,
       playerHealth: 100,
-      currentRound: 0
+      currentRound: 0,
+      winner: null
     }
   },
   computed: {
@@ -18,6 +19,26 @@ const app = Vue.createApp({
     },
     mayUseHealPlayer() {
       return this.currentRound % 3 !== 0
+    }
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // draw
+        this.winner = 'draw'
+      } else if (value <= 0) {
+        // player lost
+        this.winner = 'monster'
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // draw
+        this.winner = 'draw'
+      } else if (value <= 0) {
+        // monster lost
+        this.winner = 'player'
+      }
     }
   },
   methods: {
